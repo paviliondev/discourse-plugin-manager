069cb7faf5bebb4132d47b1523049b30ad4008a0
Robert Barrow <merefield@gmail.com>
2021-03-10 11:21:15 +0000

    FIX: dev setup script & add transfer script to make git management more controlled

--- dev_setup.sh
+++ dev_setup.sh
@@ -1,10 +1,13 @@
-mv -f lib/plugin_initialization_guard.rb lib/plugin_initialization_guard_old.rb
+CodeRoot="${CODE_ROOT:-/home/$USER/code}"
+DiscourseRoot="${DISCOURSE_ROOT:-/home/$USER/discourse}"
+
+mv -f ${DiscourseRoot}/lib/plugin_initialization_guard.rb lib/plugin_initialization_guard_old.rb
 mkdir -p lib/plugin_guard
-cp -rf ~/code/discourse-plugin-manager-server/lib/plugin_guard/error.rb lib/plugin_guard/error.rb
-cp -rf ~/code/discourse-plugin-manager-server/lib/plugin_guard/handler.rb lib/plugin_guard/handler.rb
-cp -rf ~/code/discourse-plugin-manager-server/lib/plugin_guard/log.rb lib/plugin_guard/log.rb
-cp -rf ~/code/discourse-plugin-manager-server/lib/plugin_guard/logs.rb lib/plugin_guard/logs.rb
-cp -rf ~/code/discourse-plugin-manager-server/lib/plugin_guard.rb lib/plugin_guard.rb
-cp -rf ~/code/discourse-plugin-manager-server/lib/tasks/plugin_guard.rake lib/tasks/plugin_guard.rake
-cp -rf ~/code/discourse-plugin-manager-server/lib/plugin_initialization_guard.rb lib/plugin_initialization_guard.rb
+cp -rf ${CodeRoot}/discourse-plugin-manager-server/lib/plugin_guard/error.rb lib/plugin_guard/error.rb
+cp -rf ${CodeRoot}/discourse-plugin-manager-server/lib/plugin_guard/handler.rb lib/plugin_guard/handler.rb
+cp -rf ${CodeRoot}/discourse-plugin-manager-server/lib/plugin_guard/log.rb lib/plugin_guard/log.rb
+cp -rf ${CodeRoot}/discourse-plugin-manager-server/lib/plugin_guard/logs.rb lib/plugin_guard/logs.rb
+cp -rf ${CodeRoot}/discourse-plugin-manager-server/lib/plugin_guard.rb lib/plugin_guard.rb
+cp -rf ${CodeRoot}/discourse-plugin-manager-server/lib/tasks/plugin_guard.rake lib/tasks/plugin_guard.rake
+cp -rf ${CodeRoot}/discourse-plugin-manager-server/lib/plugin_initialization_guard.rb lib/plugin_initialization_guard.rb
 mkdir -p plugins_incompatible
