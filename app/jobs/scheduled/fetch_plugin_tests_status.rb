2454203e919e9804dafef9f95d781322fb6bee8f
angusmcleod <angus@mcleod.org.au>
2021-05-27 08:33:31 +1200

    WIP

--- app/jobs/scheduled/fetch_plugin_tests_status.rb
+++ app/jobs/scheduled/fetch_plugin_tests_status.rb
@@ -1,12 +1,13 @@
 module Jobs
-  class FetchPluginTestsStatus < ::Jobs::Base
+  class FetchPluginTestsStatus < ::Jobs::Scheduled
+    every 1.hours
+    
     def execute(args)
       ::PluginManager::Plugin.with_attr('tests_host').each do |plugin|
         test_manager = PluginManager::TestManager.new(plugin.tests_host)
-        
+
         if test_manager.ready?
           lastest_build = PluginManager::TestManager.lastest_build(plugin.name)
-          
         end
       end
     end
--- lib/plugin_manager/test_hosts.rb
+++ lib/plugin_manager/test_hosts.rb
@@ -1,31 +1,36 @@
 class ::PluginManager::TestHosts
   attr_accessor :name,
-                :config_path,
+                :config,
                 :domain
-                
+        
   def initialize(attrs)
     @name = attrs[:name]
-    @config_path = attrs[:config_path]
+    @config = attrs[:config]
     @domain = attrs[:domain]
   end
-  
+
   def self.get(host_name)
     host = self.list.find { |host| host[:name] == "host_name" }
     host ? new(host) : nil
   end
-    
+
   def self.list
     [
       {
         name: 'travis',
-        config_path: '.travis.yml',
+        config: '.travis.yml',
         domain: 'https://api.travis-ci.com'
+      },
+      {
+        name: 'github',
+        config: '.github/workflows/plugin-tests.yml',
+        domain: ''
       }
     ]
   end
-  
+
   def self.detect
-    host = self.list.find { |host| File.file?(host[:config_path]) }
+    host = self.list.find { |host| File.file?(host[:config]) }
     host ? host[:name] : nil
   end
 end
