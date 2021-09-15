2d8e21c47cf16efe659f5128e090c5bac8fc4097
angusmcleod <angus@mcleod.org.au>
2021-09-15 17:23:39 +0800

    Remove class root prefix

--- lib/plugin_manager/engine.rb
+++ lib/plugin_manager/engine.rb
@@ -1,7 +1,7 @@
 module PluginManager
   NAMESPACE ||= 'plugin-manager'
 
-  class ::Engine < ::Rails::Engine
+  class Engine < ::Rails::Engine
     engine_name PluginManager::NAMESPACE
     isolate_namespace PluginManager
   end
