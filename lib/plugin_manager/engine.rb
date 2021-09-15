4f3c0f62651e8185651cdccad072ac1f9a035246
Robert Barrow <merefield@gmail.com>
2021-09-15 09:57:24 +0100

    Seperate Plugin Manager module definition in attempt to fix load error

--- lib/plugin_manager/engine.rb
+++ lib/plugin_manager/engine.rb
@@ -1,4 +1,6 @@
-class PluginManager::Engine < ::Rails::Engine
-  engine_name PluginManager::NAMESPACE
-  isolate_namespace PluginManager
+module PluginManager
+  class ::Engine < ::Rails::Engine
+    engine_name PluginManager::NAMESPACE
+    isolate_namespace PluginManager
+  end
 end
