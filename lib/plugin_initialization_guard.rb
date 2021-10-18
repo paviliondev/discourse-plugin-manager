b408cb3a6d918fc4f02180722284610244771aee
angusmcleod <angus@mcleod.org.au>
2021-10-18 16:27:02 +0800

    remove db manipulation from custom initialization guard

--- lib/plugin_initialization_guard.rb
+++ lib/plugin_initialization_guard.rb
@@ -11,29 +11,13 @@ def plugin_initialization_guard(&block)
   @activating_plugins = source_line.include?("activate_plugins")
 
   if @activating_plugins && !@extensions_applied
+    require './app/models/plugin_store_row.rb'
+    require './app/models/plugin_store.rb'
+    require './lib/enum.rb'
     Dir["./lib/plugin_guard/**/*.rb"].each { |file| require file }
-    db_name = "discourse_#{Rails.env}"
-
-    begin
-      db_exists = ActiveRecord::Base.establish_connection(
-        adapter: 'postgresql',
-        database: db_name,
-        host: 'localhost'
-      )
-      table_exists = ActiveRecord::Base.connection.table_exists?('plugin_store_rows')
-    rescue
-      db_exists = false
-      table_exists = false
-    end
-
-    if db_exists && table_exists
-      require './app/models/plugin_store_row.rb'
-      require './app/models/plugin_store.rb'
-      require './lib/enum.rb'
-      Dir["./lib/plugin_manager/**/*.rb"].each { |file| require file }
-      Discourse.singleton_class.prepend PluginGuard::DiscourseExtension
-      Plugin::Instance.prepend PluginGuard::PluginInstanceExtension
-    end
+    Dir["./lib/plugin_manager/**/*.rb"].each { |file| require file }
+    Discourse.singleton_class.prepend PluginGuard::DiscourseExtension
+    Plugin::Instance.prepend PluginGuard::PluginInstanceExtension
 
     @extensions_applied = true
   end
@@ -42,9 +26,5 @@ def plugin_initialization_guard(&block)
     block.call
   rescue => error
     PluginGuard::Error.handle(error)
-  end
-
-  if @extensions_applied
-    ActiveRecord::Base.connection_handler.clear_active_connections!
   end
 end
