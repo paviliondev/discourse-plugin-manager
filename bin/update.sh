5112f74937df0474a590284e4baee57b026c5981
angusmcleod <angus@mcleod.org.au>
2021-12-23 14:36:58 +0800

    plugin-manager-server to plugin-manager

--- bin/update.sh
+++ bin/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd /var/discourse/shared/standalone/discourse-plugin-guard && git pull
-cd /var/discourse/shared/standalone/discourse-plugin-manager-server && git pull
+cd /var/discourse/shared/standalone/discourse-plugin-manager && git pull
 cd /var/discourse && ./launcher rebuild app
