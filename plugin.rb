82450c56343c08e3f7e8ef51771285df017736cf
Angus McLeod <angus@mcleod.org.au>
2023-03-06 09:24:41 +0100

    Add auto-assign-to-maintainers setting

--- plugin.rb
+++ plugin.rb
@@ -187,7 +187,8 @@ after_initialize do
   on(:topic_created) do |topic, opts, user|
     if defined?(Assigner) == 'constant' &&
       SiteSetting.assign_enabled &&
-      topic.category&.plugin_support
+      topic.category&.plugin_support &&
+      SiteSetting.plugin_manager_auto_assign_to_maintainers
 
       if plugin_maintainer = topic.category.plugin_maintainer
         assigner = Assigner.new(topic, Discourse.system_user)
